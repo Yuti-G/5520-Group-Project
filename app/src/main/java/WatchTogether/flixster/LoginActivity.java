469d21667a4ac5f6ceb2c169868d34704f2b56d5
yutinggan <gan.yuti@gmail.com>
2021-03-21 16:36:27 -0400

    error

---
+++ app/src/main/java/WatchTogether/flixster/LoginActivity.java
@@ -0,0 +1,15 @@
+package WatchTogether.flixster;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import flixster.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
